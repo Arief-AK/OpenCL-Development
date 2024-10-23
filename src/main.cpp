c14c96d23b5f019da1d8f6ecf7a2b7742cec9225
arief-ak <mariefak22@gmail.com>
2024-10-23 13:36:10 +0200

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <CL/cl.h>
+#include <iostream>
+
+int main(int, char**){    
+    std::cout << "Hello, from OpenCLProject!\n";
+
+    cl_uint platformCount = 0;
+    clGetPlatformIDs(0, nullptr, &platformCount);
+    std::cout << "Number of OpenCL platforms: " << platformCount << std::endl;
+    
+    system("pause");
+    return 0;
+}
