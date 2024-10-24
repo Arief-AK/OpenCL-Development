c5a2af41713e59126be72b0c1c1c04614b3baf20
arief-ak <mariefak22@gmail.com>
2024-10-25 01:32:14 +0200

    added InfoDevice class in PlatformContextDevices

---
+++ src/PlatformContextDevices/include/InfoDevice.hpp
@@ -0,0 +1,19 @@
+#include <CL/cl.h>
+#include <iostream>
+
+template <typename T>
+class InfoDevice
+{
+public:
+
+    // Function to display device information
+    template <typename U>
+    static void display(cl_device_id id, cl_device_info name, std::string str);
+
+private:
+
+    // Helper function for display function
+    template <typename K>
+    void appendBitField(K info, K value, std::string name, std::string& str);
+};
+
