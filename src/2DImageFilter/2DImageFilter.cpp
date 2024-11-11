1cd290fb5ff2ec11cd842fe5568350522aabc6ab
arief-ak <mariefak22@gmail.com>
2024-11-11 11:35:31 +0100

    completed implementation for 2DImageFilter

--- src/2DImageFilter/2DImageFilter.cpp
+++ src/2DImageFilter/2DImageFilter.cpp
@@ -9,6 +9,7 @@
 // CONSTANTS
 #define PLATFORM_INDEX 0
 #define DEVICE_INDEX 0
+#define USE_MAPPING 1
 
 cl_mem LoadImage(cl_context context, char* filename, int &width, int &height){
     // Initialise format and image from file
@@ -47,6 +48,29 @@ cl_mem LoadImage(cl_context context, char* filename, int &width, int &height){
     }
 
     return cl_image;
+}
+
+bool SaveImage(char* filename, char* buffer, int width, int height, int row_pitch = 0){
+    // Retrieve format
+    FREE_IMAGE_FORMAT format = FreeImage_GetFIFFromFilename(filename);
+    auto pitch = width * 4;
+
+    if(USE_MAPPING){
+        pitch = row_pitch;
+    }
+    FIBITMAP *image = FreeImage_ConvertFromRawBits((BYTE*)buffer, width, height, width * 4, 32, 0xFF000000, 0x00FF0000, 0x0000FF00);
+
+    return (FreeImage_Save(format, image, filename) == TRUE) ? true: false;
+}
+
+size_t RoundUp(int group_size, int global_size){
+    int r = global_size % group_size;
+    
+    if(r == 0){
+        return global_size;
+    } else{
+        return global_size + group_size - r;
+    }
 }
 
 void Cleanup(cl_context context, cl_command_queue commandQueue, cl_program program, cl_kernel kernel, cl_mem memObjects[3]){
@@ -110,11 +134,11 @@ int main(int argc, char** argv)
 
     // Load input image from file and load it into an OpenCL image object
     cl_int err_num;
-    cl_int width, height;
+    int width, height;
     cl_mem image_objects[2] = {0, 0};
 
     // TODO: Change this back to argv[1]
-    image_objects[0] = LoadImage(context, "../../../../src/2DImageFilter/image/blurry_photo.jpeg", width, height);
+    image_objects[0] = LoadImage(context, "blurry_photo.jpeg", width, height);
     // image_objects[0] = LoadImage(context, argv[1], width, height);
     if (image_objects[0] == 0){
         std::cerr << "Error loading: " << std::string(argv[1]) << std::endl;
@@ -125,7 +149,12 @@ int main(int argc, char** argv)
     cl_image_format clImageFormat;
     clImageFormat.image_channel_order = CL_RGBA;
     clImageFormat.image_channel_data_type = CL_UNORM_INT8;
-    image_objects[1] = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &clImageFormat, width, height, 0, NULL, &err_num);
+
+    if(USE_MAPPING){
+        image_objects[1] = clCreateImage2D(context, CL_MEM_READ_WRITE, &clImageFormat, width, height, 0, NULL, &err_num);
+    } else{
+        image_objects[1] = clCreateImage2D(context, CL_MEM_WRITE_ONLY, &clImageFormat, width, height, 0, NULL, &err_num);
+    }
     if (err_num != CL_SUCCESS){
         std::cerr << "Error creating CL output image object." << std::endl;
         return 1;
@@ -137,7 +166,7 @@ int main(int argc, char** argv)
     if(err_num != CL_SUCCESS){
         std::cerr << "Error creating OpenCL sampler object" << std::endl;
         // TODO: Cleanup
-        return -1;
+        return 1;
     }
     std::cout << "Succesfully created a sampler object" << std::endl;
 
@@ -149,9 +178,66 @@ int main(int argc, char** argv)
     err_num |= clSetKernelArg(kernel, 4, sizeof(cl_int), &height);
     if(err_num != CL_SUCCESS){
         std::cerr << "Error setting kernel arguments." << std::endl;
-        return -1;
+        return 1;
     }
     std::cout << "Successfully set kernel arguments" << std::endl;
+
+    // Initialise the work-size
+    size_t local_work_size[2] = {16, 16};
+    size_t global_work_size[2] = {RoundUp(local_work_size[0], width), RoundUp(local_work_size[1], height)};
+
+    // Execute the kernel
+    err_num = clEnqueueNDRangeKernel(command_queue, kernel, 2, NULL, global_work_size, local_work_size, 0, NULL, NULL);
+    if(err_num != CL_SUCCESS){
+        std::cerr << "Error executing the kernel" << std::endl;
+        // TODO: Cleanup
+        return 1;
+    }
+    std::cout << "Successfully executed kernel" << std::endl;
+
+    // Read the output buffer back from device to host memory
+    size_t origin[3] = {0, 0, 0};
+    size_t region[3] = {width, height, 1};
+    size_t row_pitch = 0;
+    char* buffer = new char [width * height * 4];
+    err_num = clEnqueueReadImage(command_queue, image_objects[1], CL_TRUE, origin, region, 0, 0, buffer, 0, NULL, NULL);
+
+    if(USE_MAPPING){
+        buffer = (char*) clEnqueueMapImage(command_queue, image_objects[1], CL_TRUE, CL_MAP_READ, origin, region, &row_pitch, NULL, 0, NULL, NULL, &err_num);
+    } else{
+        err_num = clEnqueueReadImage(command_queue, image_objects[1], CL_TRUE, origin, region, 0, 0, buffer, 0, NULL, NULL);
+    }
+    if(err_num != CL_SUCCESS){
+        std::cerr << "Error reading the result buffer" << std::endl;
+        // TODO: Cleanup
+        return 1;
+    }
+    std::cout << "Successfully read the result buffer" << std::endl;
+
+    // Saving the image
+    auto result = false;
+    if(USE_MAPPING){
+        result = SaveImage("edited.jpeg", buffer, width, height, row_pitch);
+    } else{
+        result = SaveImage("edited.jpeg", buffer, width, height);
+    }
+    if(!result){
+        std::cerr << "Failed to save image to edited.jpeg" << std::endl;
+        // TODO: Cleanup
+        return 1;
+    }
+    std::cout << "Successfully saved image to edited.jpeg" << std::endl;
+
+    if(USE_MAPPING){
+        // Unmap the image buffer
+        err_num = clEnqueueUnmapMemObject(command_queue, image_objects[1], buffer, 0, NULL, NULL);
+        std::cout << "Successfully unmaped image buffer" << std::endl;
+    }
+    if(err_num != CL_SUCCESS){
+        std::cerr << "Failed to unmap the result buffer" << std::endl;
+        // TODO: Cleanup
+        return 1;
+    }
 
     FreeImage_DeInitialise();
     std::cout << "\nProgram executed succesfully" << std::endl;
